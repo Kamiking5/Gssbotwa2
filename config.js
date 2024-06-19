6e865bd0ad23ee639a70819480d389ba30395561
Kamiking5 <145831898+Kamiking5@users.noreply.github.com>
2024-06-19 16:33:53 +0500

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaWnznVKbYMT3YKacj0J'
+global.linkGroup = 'https://whatsapp.com/channel/0029VaWnznVKbYMT3YKacj0J'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
-global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.ownername = "Kami_Broken";
+global.owner = ['923086679730'];
+global.premium = ['923086679730'];
+global.packname = "Mr Kami";
+global.author = "Kami_Broken";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
@@ -24,8 +24,8 @@ global.autoRecord = false
 global.autoreact = false
 global.onlygroup = false
 global.onlypc = false
-global.welcome = true
-global.autoBlock = true
+global.welcome = false
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
